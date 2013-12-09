2eae68c30d8b554d253b428d4a9682745880446a
Tohru Kobayashi <soultoru@gmail.com>
2013-12-09 05:08:20 +0000

    rails commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Datadrain::Application.config.session_store :cookie_store, key: '_datadrain_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Datadrain::Application.config.session_store :active_record_store
